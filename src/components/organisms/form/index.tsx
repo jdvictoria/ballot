9a53988c0c94c6f93acffa4cf69a1b513462a4c2
jdvictoria <jdvictoria.tech@gmail.com>
2023-10-30 11:59:29 +0800

    Finalize Form Header Design

--- src/components/organisms/form/index.tsx
+++ src/components/organisms/form/index.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import styled from "styled-components";
 
+import {FormHeader} from "../../molecules/header";
+
 const FormContainer = styled.div`
   justify-content: center;
   align-items: center;
+  flex-direction: row;
 
   margin-right: 7.5px;
   
   width: 47.5%;
   height: 92.5%;
+`
+
+const InputContainer = styled.div`
+  justify-content: center;
+  align-items: center;
   
-  background-color: aquamarine;
+  width: 100%;
+  height: 40%;
+  
+  background-color: yellow;
+`
+
+const SubmissionContainer = styled.div`
+  justify-content: center;
+  align-items: center;
+  
+  width: 100%;
+  height: 40%;
+  
+  background-color: blue;
 `
 
 export function Form() {
     return (
         <FormContainer>
+            <FormHeader/>
+            <InputContainer>
 
+            </InputContainer>
+            <SubmissionContainer>
+
+            </SubmissionContainer>
         </FormContainer>
     )
 }
