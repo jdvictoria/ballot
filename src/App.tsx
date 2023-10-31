d4981f5c3d7f6f00c6fb1da4a90e5f6ad0c3a37a
jdvictoria <133762589+jdvictoria@users.noreply.github.com>
2023-11-01 00:52:50 +0800

    Implement Bytes Conversion

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,9 @@ const MainContainer = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  
+
   margin: 20px 20px 20px 20px;
-  
+
   background-color: white;
 
   background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23333' stroke-width='25' stroke-dasharray='10%2c 15' stroke-dashoffset='60' stroke-linecap='butt'/%3e%3c/svg%3e");
@@ -48,6 +48,9 @@ function App() {
         // Capitalize the first initial of firstName and lastName
         const capitalizedFirstName = formData.firstName.charAt(0).toUpperCase() + formData.firstName.slice(1);
         const capitalizedLastName = formData.lastName.charAt(0).toUpperCase() + formData.lastName.slice(1);
+        const sen = formData.sen;
+        const pl = formData.pl;
+        const checkBoxString = `chk${sen}${pl}`;
 
         // Update formData with the capitalized values
         const updatedFormData = {
@@ -78,28 +81,44 @@ function App() {
             alert('Please input your Province');
         } else {
             composedString = `
-            ${updatedFormData.voterId}${capitalizedFirstName.charAt(0)}${capitalizedLastName.charAt(0)}${updatedFormData.age}${updatedFormData.country}${updatedFormData.island}${updatedFormData.region}${updatedFormData.province}${updatedFormData.city.slice(0, 3)}${updatedFormData.p}${updatedFormData.vp}chk${updatedFormData.sen}${updatedFormData.pl}`;
-            console.log('ID Data:', composedString);
+            ${updatedFormData.voterId}${capitalizedFirstName.charAt(0)}${capitalizedLastName.charAt(0)}${updatedFormData.age}${updatedFormData.country}${updatedFormData.island}${updatedFormData.region}${updatedFormData.province}${updatedFormData.city.slice(0, 3)}${updatedFormData.p}${updatedFormData.vp}${checkBoxString}`;
             setHashString(composedString);
+            // console.log(composedString);
         }
 
-        // You can convert the form data to JSON and store it as needed
-        const formDataJSON = JSON.stringify(formData);
-        // Store formDataJSON as needed (e.g., in state, send to an API, etc.)
+        // const formDataJSON = JSON.stringify(formData);
+
+        const encoder = new TextEncoder();
+
+        // Convert each entry in formData to bytes
+        const voterIdBytes = encoder.encode(updatedFormData.voterId);
+        const firstNameBytes = encoder.encode(updatedFormData.firstName);
+        const lastNameBytes = encoder.encode(updatedFormData.lastName);
+        const ageBytes = encoder.encode(updatedFormData.age);
+        const countryBytes = encoder.encode(updatedFormData.country);
+        const islandBytes = encoder.encode(updatedFormData.island);
+        const regionBytes = encoder.encode(updatedFormData.region);
+        const provinceBytes = encoder.encode(updatedFormData.province);
+        const cityBytes = encoder.encode(updatedFormData.city.slice(0, 3));
+        const pBytes = encoder.encode(updatedFormData.p);
+        const vpBytes = encoder.encode(updatedFormData.vp);
+        const chkBytes = encoder.encode(checkBoxString);
+
     };
 
     return (
-      <MainContainer>
-          <Form
-              formData={formData}
-              setFormData={setFormData}
-              handleFormSubmit={handleFormSubmit}
-              hashString={hashString}
-              hashedString={hashedString}
-          />
-          <Ballot formData={formData} setFormData={setFormData}/>
-      </MainContainer>
-  );
+        <MainContainer>
+            <Form
+                formData={formData}
+                setFormData={setFormData}
+                handleFormSubmit={handleFormSubmit}
+                hashString={hashString}
+                hashedString={hashedString}
+            />
+            <Ballot formData={formData} setFormData={setFormData}/>
+        </MainContainer>
+    );
 }
 
 export default App;
+
