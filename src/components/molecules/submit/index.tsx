92c0b6935502f4fd2b5f5c5305215e3c93b39be3
jdvictoria <133762589+jdvictoria@users.noreply.github.com>
2023-11-03 12:20:31 +0800

    Implement Metamask Connection

--- src/components/molecules/submit/index.tsx
+++ src/components/molecules/submit/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
 
+import { MetaMaskButton } from "@metamask/sdk-react-ui";
+
 const SubmissionContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -18,6 +20,24 @@ const TextSection = styled.div`
   flex-direction: column;
   
   width: 100%;
+`
+
+const MetamaskSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  
+  width: 100%;
+`
+
+const GridColumn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 50%;
 `
 
 const StyledText = styled.text`
@@ -46,13 +66,23 @@ export function FormOutput({hashString, hashedString}) {
                 <StyledText style={{fontWeight: "bold"}}>
                     {!hashedString ? 'N/A' : hashedString}
                 </StyledText>
-                <StyledText>
-                    Deployment Status:
-                </StyledText>
-                <StyledText style={{fontWeight: "bold"}}>
-                    N/A
-                </StyledText>
             </TextSection>
+            <MetamaskSection>
+                <GridColumn>
+                    <StyledText>
+                        Connection Status:
+                    </StyledText>
+                    <MetaMaskButton theme={"light"} color="white" connectedText={'Connected'}></MetaMaskButton>
+                </GridColumn>
+                <GridColumn>
+                    <StyledText>
+                        Deployment Status:
+                    </StyledText>
+                    <StyledText style={{fontWeight: "bold"}}>
+                        N/A
+                    </StyledText>
+                </GridColumn>
+            </MetamaskSection>
         </SubmissionContainer>
     )
 }
